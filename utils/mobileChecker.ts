c725dee7b56146d44da6caf00e307f2ee1fe811f
hwang <nmc2711@naver.com>
2022-07-10 20:33:52 +0900

    mobile checker modifyt

--- utils/mobileChecker.ts
+++ utils/mobileChecker.ts
@@ -1,22 +1,12 @@
 
-export default {
-    Android() {
-      return navigator.userAgent.match(/Android/i) === null ? false : true;
-    },
-    BlackBerry() {
-      return navigator.userAgent.match(/BlackBerry/i) === null ? false : true;
-    },
-    IOS() {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i) === null ? false : true;
-    },
-    Opera() {
-      return navigator.userAgent.match(/Opera Mini/i) === null ? false : true;
-    },
-    Windows() {
-      return navigator.userAgent.match(/IEMobile/i) === null ? false : true;
-    },
-    any() {
-      return (this.Android() || this.BlackBerry() || this.IOS() || this.Opera() || this.Windows());
-    }
-  };
-  
+ function isMobile(): boolean {
+  const agent = navigator.userAgent;
+  if (agent === undefined) return false;
+
+  const mobile1 = agent.match(/.*(iPhone|iPod|iPad|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson).*/g);
+  const mobile2 = agent.match(/.*(LG|SAMSUNG|Samsung).*/g);
+
+  return !!(mobile1 || mobile2);
+}
+
+export default isMobile;
